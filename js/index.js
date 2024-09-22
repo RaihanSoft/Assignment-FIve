12a0b8fddba775309ddcb5311fada5f79f2e0d32
Raihan <aboraihan14@gmail.com>
2024-09-23 02:10:24 +0600

    validation done

--- js/index.js
+++ js/index.js
@@ -3,21 +3,52 @@
 // const btnDonate = document.getElementById('btnDonate')
 // const donatedBalance = document.getElementById('donatedBalance')
 
+// const special = document.getElementById('special')
+
 
 document.getElementById('btnDonate').addEventListener('click', function () {
 
+    // !special Model
+    const special = document.getElementById('specialModel')
+    special.classList.remove('hidden')
+    special.classList.add('flex')
+    document.getElementById('btnClose').addEventListener('click', function () {
+        special.classList.add('hidden')
+
+    })
+
+
+
+
+      // ! validation One
     const inpDonateAmount = Donate('inpDonateAmount')
+    if (isNaN(inpDonateAmount) || inpDonateAmount === 0){
+            document.getElementById('specialModel').classList.add('hidden')
+            alert("Invalid Input")
+        return
+    
+    }
 
+
+
+
+    // !function calls 
     const mainBalance = DonateInner('mainBalance')
-
     const donatedBalance = DonateInner('donatedBalance')
-
     const minusNewBalance = mainBalance - inpDonateAmount
     document.getElementById('mainBalance').innerText = minusNewBalance
-
-
     const plusNewBalance = donatedBalance + inpDonateAmount
     donatedBalance = document.getElementById('donatedBalance').innerText = plusNewBalance
+
+
+  
+
+
+
+
+
+
+
 
 
 
